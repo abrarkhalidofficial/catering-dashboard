2e3de55c8bca79df7dc4eff02a3c692b186cbf1a
ibilalchaudhary <bilalchaudhary478@gmail.com>
2021-11-10 17:54:00 +0500

    customer done

--- src/Screens/Customer.js
+++ src/Screens/Customer.js
@@ -1,18 +1,43 @@
 import React from "react";
 import Button from "../Components/Button";
+import EntryHeadingRow from "../Components/EntryHeadingRow";
+import EntryInfoRow from "../Components/EntryInfoRow";
 
 export default function Customer() {
+  const tableHeadingRow = [
+    { heading: "Profile" },
+    { heading: "Name" },
+    { heading: "Last Name" },
+    { heading: "Email" },
+    { heading: "Phone" },
+  ];
+
+  const tableInfoRow = [
+    { type: "img" },
+    { info: "Fahad" },
+    { info: "Ayyaz" },
+    { info: "abc123@gmail.com" },
+    { info: "0321-3741371" },
+  ];
   return (
     <div className="container">
       <div className="container__header">
-        <div className="container__header__heading">Customers</div>
+        <div className="container__header__heading">Customer</div>
         <div className="container__header__btns">
           <Button label="Add" style={{ marginRight: "1em" }} />
           <Button label="Delete" variant="secondary" />
         </div>
       </div>
-      <div className="customer__container">
-        sdfkjsaldkfkjlsaldfjsald;fjsla;dkjflsakdjf;lsajdf;lsjdafl;kj
+      <div className="branch__container">
+        <EntryHeadingRow tableHeadingEntryRow={tableHeadingRow} />
+        <EntryInfoRow tableEntryInfoRow={tableInfoRow} />
+        <EntryInfoRow tableEntryInfoRow={tableInfoRow} />
+        <EntryInfoRow tableEntryInfoRow={tableInfoRow} />
+        <EntryInfoRow tableEntryInfoRow={tableInfoRow} />
+        <EntryInfoRow tableEntryInfoRow={tableInfoRow} />
+        <EntryInfoRow tableEntryInfoRow={tableInfoRow} />
+        <EntryInfoRow tableEntryInfoRow={tableInfoRow} />
+        <EntryInfoRow tableEntryInfoRow={tableInfoRow} />
       </div>
     </div>
   );
