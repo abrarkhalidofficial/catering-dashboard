a91a6c329dffeea9e658517aaf2185c020379d60
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2021-11-10 17:54:33 +0500

    working on dashbaord

---
+++ src/Components/PieChart.js
@@ -0,0 +1,49 @@
+import React, { PureComponent } from "react";
+import {
+  PieChart,
+  Pie,
+  Sector,
+  Cell,
+  ResponsiveContainer,
+  Tooltip,
+} from "recharts";
+
+const data = [
+  { name: "Group A", value: 400 },
+  { name: "Group B", value: 300 },
+  { name: "Group C", value: 300 },
+  { name: "Group D", value: 200 },
+];
+const COLORS = ["#508FF4", "#00C49F", "#FFBB28", "#FB6A3B"];
+
+export default class CustomePieChart extends PureComponent {
+  static demoUrl =
+    "https://codesandbox.io/s/pie-chart-with-padding-angle-7ux0o";
+
+  render() {
+    return (
+      <ResponsiveContainer width="30%" height={300}>
+        <PieChart width={400} height={400} onMouseEnter={this.onPeEnter}>
+          <Tooltip />
+          <Pie
+            data={data}
+            cx={120}
+            cy={200}
+            innerRadius={60}
+            outerRadius={80}
+            fill="#8884d8"
+            paddingAngle={5}
+            dataKey="value"
+          >
+            {data.map((entry, index) => (
+              <Cell
+                key={`cell-${index}`}
+                fill={COLORS[index % COLORS.length]}
+              />
+            ))}
+          </Pie>
+        </PieChart>
+      </ResponsiveContainer>
+    );
+  }
+}
--- src/Screens/Dashboard.js
+++ src/Screens/Dashboard.js
@@ -1,5 +1,9 @@
 import React from "react";
+import BarChart from "../Components/BarChart";
 import Button from "../Components/Button";
+import LineChart from "../Components/LineChart";
+import PieChart from "../Components/PieChart";
+import cardBackground from "../Assets/cardBackground.png";
 
 export default function Dashboard() {
   return (
@@ -12,7 +16,44 @@ export default function Dashboard() {
         </div>
       </div>
       <div className="dashboard__container">
-        sdfkjsaldkfkjlsaldfjsald;fjsla;dkjflsakdjf;lsajdf;lsjdafl;kj
+        <div className="dashboard__container__top">
+          <LineChart />
+        </div>
+
+        <div className="dashboard__container__bottom">
+          <div className="dashboard__container__card dashboard__container__card__primary">
+            <img
+              src={cardBackground}
+              alt="cardBackground"
+              className="dashboard__container__card__img"
+            />
+            <div className="dashboard__container__card__content">
+              <div className="dashboard__container__card__heading">
+                Total Profit
+              </div>
+              <div className="dashboard__container__card__value">$12,900</div>
+            </div>
+          </div>
+          <div className="dashboard__container__card dashboard__container__card__secondary">
+            <img
+              src={cardBackground}
+              alt="cardBackground"
+              className="dashboard__container__card__img"
+            />
+            <div className="dashboard__container__card__content">
+              <div className="dashboard__container__card__heading">
+                Total expense
+              </div>
+              <div className="dashboard__container__card__value">$12,900</div>
+            </div>
+          </div>
+          <div className="dashboard__container__card">
+            <BarChart />
+          </div>
+          <div className="dashboard__container__card">
+            <PieChart />
+          </div>
+        </div>
       </div>
     </div>
   );
