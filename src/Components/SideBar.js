0b8719b7e111f2023a81f4ff069460c3108a8e4a
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2021-11-11 12:04:07 +0500

    side bar fix

--- src/Components/SideBar.js
+++ src/Components/SideBar.js
@@ -130,68 +130,75 @@ export default function SideBar({ isLogedin, setIsLogedin }) {
     <div className="side__bar">
       <div className="side__bar__header">
         <div className="side__bar__header__left">
-          {isInPanel ? (
-            <IconBtn
-              svg={
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16.828">
-                  <g id="arrow-left" transform="translate(-4 -3.586)">
-                    <line
-                      id="Line_11"
-                      data-name="Line 11"
-                      x1="14"
-                      transform="translate(5 12)"
-                      fill="none"
-                      stroke="#fb6a3b"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                    />
-                    <path
-                      id="Path_9210"
-                      data-name="Path 9210"
-                      d="M12,19,5,12l7-7"
-                      fill="none"
-                      stroke="#fb6a3b"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                    />
-                  </g>
-                </svg>
-              }
-              onClick={() => {
-                setIsInPanel(false);
-                setIsOnNotificationPenel(false);
-              }}
-            />
-          ) : (
+          {isLogedin ? null : (
             <>
-              <IconBtn
-                svg={
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 18.183 22.16"
-                  >
-                    <path
-                      id="Icon_material-notifications"
-                      data-name="Icon material-notifications"
-                      d="M15.091,25.91a2.28,2.28,0,0,0,2.273-2.273H12.819A2.272,2.272,0,0,0,15.091,25.91Zm6.819-6.819V13.41C21.91,9.921,20.046,7,16.8,6.227V5.455a1.7,1.7,0,0,0-3.409,0v.773C10.125,7,8.273,9.909,8.273,13.41v5.682L6,21.365V22.5H24.183V21.365Z"
-                      transform="translate(-6 -3.75)"
-                      fill="#fb6a3b"
-                    />
-                  </svg>
-                }
-                onClick={() => {
-                  setIsInPanel(true);
-                  setIsOnNotificationPenel(true);
-                }}
-              />
-              <Avatar
-                userPic={userPic}
-                onClick={() => {
-                  setIsInPanel(true);
-                }}
-              />
+              {isInPanel ? (
+                <IconBtn
+                  svg={
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 16 16.828"
+                    >
+                      <g id="arrow-left" transform="translate(-4 -3.586)">
+                        <line
+                          id="Line_11"
+                          data-name="Line 11"
+                          x1="14"
+                          transform="translate(5 12)"
+                          fill="none"
+                          stroke="#fb6a3b"
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          stroke-width="2"
+                        />
+                        <path
+                          id="Path_9210"
+                          data-name="Path 9210"
+                          d="M12,19,5,12l7-7"
+                          fill="none"
+                          stroke="#fb6a3b"
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          stroke-width="2"
+                        />
+                      </g>
+                    </svg>
+                  }
+                  onClick={() => {
+                    setIsInPanel(false);
+                    setIsOnNotificationPenel(false);
+                  }}
+                />
+              ) : (
+                <>
+                  <IconBtn
+                    svg={
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 18.183 22.16"
+                      >
+                        <path
+                          id="Icon_material-notifications"
+                          data-name="Icon material-notifications"
+                          d="M15.091,25.91a2.28,2.28,0,0,0,2.273-2.273H12.819A2.272,2.272,0,0,0,15.091,25.91Zm6.819-6.819V13.41C21.91,9.921,20.046,7,16.8,6.227V5.455a1.7,1.7,0,0,0-3.409,0v.773C10.125,7,8.273,9.909,8.273,13.41v5.682L6,21.365V22.5H24.183V21.365Z"
+                          transform="translate(-6 -3.75)"
+                          fill="#fb6a3b"
+                        />
+                      </svg>
+                    }
+                    onClick={() => {
+                      setIsInPanel(true);
+                      setIsOnNotificationPenel(true);
+                    }}
+                  />
+                  <Avatar
+                    userPic={userPic}
+                    onClick={() => {
+                      setIsInPanel(true);
+                    }}
+                  />
+                </>
+              )}
             </>
           )}
         </div>
